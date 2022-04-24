ddee981dd6978fcb9831ce36fe7d50708f8ab491
fabioRocha231 <fabioharoldo1@gmail.com>
2022-04-24 18:38:27 -0300

    fix: error find, now just figure out how to block the action when category already exsits

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -6,9 +6,8 @@ import { Category } from "../entities/Category";
 export class CreateCategoryService implements ICreateCategoryService {
   async execute({ name, description }: CategoryRequest): Promise<Category | Error> {
     const repo = AppDataSource.getRepository(Category);
-    console.log(repo, "repo");
-    const check = await repo.findOneByOrFail({ name });
-    if (check) return new Error("Category already exists");
+    const check = await repo.findOne({ where: { name } });
+    if (check instanceof Error) return new Error("Category already exists");
 
     console.log(name, description);
 
